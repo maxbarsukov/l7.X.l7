c73b06228817dc2f86a81f2c46b2e6857e9f7ec7
maxbarsukov <maximbarsukov@bk.ru>
2022-05-03 23:09:43 +0300

    feat: :sparkles: improve cli

--- src/Cli.php
+++ src/Cli.php
@@ -2,18 +2,36 @@
 
 namespace Maxbarsukov\L7xl7;
 
+use Maxbarsukov\L7xl7\FileDispatcher\FileDispatcher;
+
 class Cli
 {
     private array $_config;
 
-    public function __construct(array $args)
+    public function __construct(array $argv)
     {
-        $this->_config = $args;
+        $this->_config = $argv;
     }
 
     public function run()
     {
-        echo Dictionary::keywords()['функция']."\n";
-        echo Dictionary::signs()['₽']."\n";
+        $baseFilename = $this->_config[0];
+        $filename = $_SERVER['PWD'].'/'.$baseFilename;
+
+        if (is_file($baseFilename) || is_dir($baseFilename)) {
+            var_dump(
+                (new FileDispatcher())
+                    ->dispatch($baseFilename)
+            );
+            return;
+        }
+        if (is_file($filename) || is_dir($filename)) {
+            var_dump(
+                (new FileDispatcher())
+                    ->dispatch($filename)
+            );
+            return;
+        }
+        Error::raise('No such file or directory: '.$filename);
     }
 }
