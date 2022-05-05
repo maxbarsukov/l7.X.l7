d8b153526461f470fac56edf2cb4e2edc066425a
maxbarsukov <maximbarsukov@bk.ru>
2022-05-05 22:22:23 +0300

    feat: :sparkles: add help command to CLI

--- src/Cli.php
+++ src/Cli.php
@@ -9,22 +9,28 @@ use Maxbarsukov\L7xl7\Utils\PathTransformer;
 
 class Cli
 {
-    private array $_config;
+    private ?string $_filename;
+    private string $_outDir;
+    private array $_fileExt;
 
     public function __construct(array $argv)
     {
-        $this->_config = $argv;
+        $this->_parseConfig($argv);
     }
 
     public function run(): void
     {
-        $baseFilename = $this->_config[0];
+        if ('-h' === $this->_filename || '--help' === $this->_filename) {
+            $this->_help();
+        }
+
+        $baseFilename = $this->_filename;
         $filename = $_SERVER['PWD'].'/'.$baseFilename;
 
         foreach ([$baseFilename, $filename] as $f) {
             if (is_file($f) || is_dir($f)) {
-                $filenames = (new FileLoader(['.ruphp']))->load($f);
-                $pathTransformer = new PathTransformer('/out');
+                $filenames = (new FileLoader($this->_fileExt))->load($f);
+                $pathTransformer = new PathTransformer($this->_outDir);
                 $dispatcher = new FileDispatcher($filenames, $pathTransformer);
                 $dispatcher->transpileAndSave();
 
@@ -32,6 +38,62 @@ class Cli
             }
         }
 
-        Error::raise('No such file or directory: '.$filename);
+        Error::raise('Нет такого файла или папки: '.$filename);
+    }
+
+    private function _help(): void
+    {
+        echo <<<HELP
+        Description:
+          Display help for a command
+
+        Usage:
+          l7xl7 [<input_dir_or_file>] [options]
+
+        Arguments:
+          input_dir_or_file       Input file or directory
+
+        Options:
+              --inpext=EXTS       The input files extensions
+                                      (format as ".ext,..." or "ext,...")
+                                      [default: "ruphp,rup"]
+              --outdir=OUTDIR     The directory for compiled files
+                                      [default: "out"]
+
+        Help:
+          This help message:
+
+            l7xl7 help
+
+        HELP;
+        exit;
+    }
+
+    private function _parseConfig(array $args): void
+    {
+        if (empty($args)) {
+            return;
+        }
+        $this->_filename = $args[0];
+        foreach (\array_slice($args, 1) as $arg) {
+            if (str_starts_with($arg, '--inpext=')) {
+                $s = $this->_removePrefix($arg, '--inpext=');
+                $this->_fileExt = explode(',', $s);
+            }
+            if (str_starts_with($arg, '--outdir=')) {
+                $this->_outDir = $this->_removePrefix($arg, '--outdir=');
+            }
+        }
+        if (!isset($this->_fileExt)) {
+            $this->_fileExt = ['ruphp', 'rphp'];
+        }
+        if (!isset($this->_outDir)) {
+            $this->_outDir = '/out';
+        }
+    }
+
+    private function _removePrefix(string $text, string $prefix): string
+    {
+        return substr($text, \strlen($prefix)).'';
     }
 }
