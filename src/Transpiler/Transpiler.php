27a1b783c7c8c5fb5f8d7e7e9afe15b82e4b874a
maxbarsukov <maximbarsukov@bk.ru>
2022-05-06 18:57:33 +0300

    feat: :sparkles: ignore strings and comments at translation

--- src/Transpiler/Transpiler.php
+++ src/Transpiler/Transpiler.php
@@ -5,11 +5,22 @@ namespace Maxbarsukov\L7xl7\Transpiler;
 use Maxbarsukov\L7xl7\Dictionary\Dictionary;
 use Maxbarsukov\L7xl7\Parser\Parser;
 use Maxbarsukov\L7xl7\Utils\Error;
+use Maxbarsukov\L7xl7\Utils\Utils;
 
 class Transpiler
 {
     private string $_text;
     private string $_filename;
+
+    // Hashtag comments
+    private array $_commentsH = [];
+    // One line comments
+    private array $_commentsO = [];
+    // Multiline comments
+    private array $_commentsM = [];
+
+    private array $_stringsDC = [];
+    private array $_stringsUC = [];
 
     public function __construct(string $filename)
     {
@@ -19,11 +30,102 @@ class Transpiler
 
     public function transpile(): string
     {
+        $this->_removeStrings();
+        $this->_removeComments();
+
         $this->_checkForDollarSigns();
         $this->_updateKeywords();
         $this->_updateAst();
 
+        $this->_putStringsBack();
+        $this->_putCommentsBack();
+
         return $this->_text;
+    }
+
+    private function _removeStrings()
+    {
+        $count = 0;
+        $this->_text = preg_replace_callback(
+            "/\".*?(?<!\\\)\"/",
+            function ($matches) use (&$count) {
+                $val = '"LXL_STRING_D_'.(string) $count.'"';
+                $this->_stringsDC[$count] = $matches[0];
+                ++$count;
+
+                return $val;
+            },
+            $this->_text
+        );
+
+        $count = 0;
+        $this->_text = preg_replace_callback(
+            "/'.*?(?<!\\\)'/",
+            function ($matches) use (&$count) {
+                $val = '"LXL_STRING_U_'.(string) $count.'"';
+                $this->_stringsUC[$count] = $matches[0];
+                ++$count;
+
+                return $val;
+            },
+            $this->_text
+        );
+    }
+
+    private function _putStringsBack()
+    {
+        foreach (['D', 'U'] as $char) {
+            $this->_text = preg_replace_callback(
+                '/"LXL_STRING_'.$char.'_(.*?)"/',
+                function ($matches) use ($char) {
+                    $tmp = Utils::removePrefix($matches[0], '"LXL_STRING_'.$char.'_');
+                    $tmp = substr_replace($tmp, '', -1);
+                    $val = (int) $tmp;
+
+                    return $this->{'_strings'.$char.'C'}[$val];
+                },
+                $this->_text
+            );
+        }
+    }
+
+    private function _removeComments()
+    {
+        $commentTypes = [
+            'H' => "~#[^\r\n]*~",
+            'O' => "~//[^\r\n]*~",
+            'M' => "~/\*.*?\*/~s",
+        ];
+        foreach ($commentTypes as $key => $regex) {
+            $count = 0;
+            $this->_text = preg_replace_callback(
+                $regex,
+                function ($matches) use (&$count, $key) {
+                    $val = '"LXL_COMMENT_'.$key.'_'.(string) $count.'";';
+                    $this->{'_comments'.$key}[$count] = $matches[0];
+                    ++$count;
+
+                    return $val;
+                },
+                $this->_text
+            );
+        }
+    }
+
+    private function _putCommentsBack()
+    {
+        foreach (['H', 'O', 'M'] as $char) {
+            $this->_text = preg_replace_callback(
+                '/"LXL_COMMENT_'.$char.'_(.*?)";/',
+                function ($matches) use ($char) {
+                    $tmp = Utils::removePrefix($matches[0], '"LXL_COMMENT_'.$char.'_');
+                    $tmp = substr_replace($tmp, '', -2);
+
+                    return $this->{'_comments'.$char}[(int) $tmp];
+                },
+                $this->_text
+            );
+        }
     }
 
     private function _updateAst(): void
@@ -33,7 +135,6 @@ class Transpiler
 
     private function _updateKeywords(): void
     {
-        // TODO: Ignore comments and strings
         $dictionary = Dictionary::load();
 
         $from = array_keys($dictionary);
